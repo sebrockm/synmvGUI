3d173c8e0353a7ef230be31d79f2f4e7d76e880a
sebrockm <sebrockm@uos.de>
2014-04-19 23:08:05 +0200

    moved buttons to menubar and replaced config-system

--- src/SynmvFrame.java
+++ src/SynmvFrame.java
@@ -1,3 +1,4 @@
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,10 +13,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -26,27 +29,17 @@ public class SynmvFrame extends JFrame {
 
 	private final JPanel jobcontainer = new JPanel();
 	private final JLabel label = new JLabel();
-	private final JButton loadFile = new JButton("load jobs");
-	private final JButton loadConfig = new JButton("load schedule");
-	private final JButton storeConfig = new JButton("save schedule");
+	private final JMenuBar menubar = new JMenuBar();
+	private final JMenu fileMenu = new JMenu("File");
+	private final JMenuItem loadFile = new JMenuItem("load jobs");
+	private final JMenuItem storeFile = new JMenuItem("save jobs");
 	private SynmvJob[] jobs = new SynmvJob[0];
 	
 	private final JScrollPane scroll = new JScrollPane();
 	private final JFileChooser fileChooser = new JFileChooser();
-	private final JFileChooser configChooser = new JFileChooser();
 	
-	private class ReadJobsFirstException extends Exception {
-		public ReadJobsFirstException() {
-			super();
-		}
-		@SuppressWarnings("unused")
-		public ReadJobsFirstException(String message) {
-			super(message);
-		}
-	}
-	
+	@SuppressWarnings("unused")	
 	private class InvalidFileFormatException extends Exception {
-		@SuppressWarnings("unused")
 		public InvalidFileFormatException() {
 			super();
 		}
@@ -137,24 +130,24 @@ public class SynmvFrame extends JFrame {
 		return retjobs;
 	}
 	
-	private void storeConfigToFile(String filename) {
+	private void storeJobsToFile(String filename) {
 		BufferedWriter writer = null;
 		
 		try {
 			writer = new BufferedWriter(new FileWriter(filename));
 			
-			SynmvJob tmp = jobs[0];
-			while(tmp.getPred() != null) {
-				tmp = tmp.getPred();
-			}
-			int i;
-			for(i = 0; i < jobs.length && tmp != null; i++) {
-				writer.write(tmp.getID() + " " + (i+1));
+			SynmvJob tmp = jobs[0].getFirstPredecessor();
+			
+			writer.write(tmp.getMachineCount() + " " + jobs.length);
+			writer.newLine();
+			
+			while(tmp != null) {
+				for(int i = 0; i < tmp.getMachineCount(); i++) {
+					writer.write(tmp.getTime(i) + " ");
+				}
 				writer.newLine();
+				
 				tmp = tmp.getNext();
-			}
-			if(tmp != null || i != jobs.length) {
-				throw new RuntimeException("something seriously went wrong");
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -169,81 +162,6 @@ public class SynmvFrame extends JFrame {
 		}
 	}
 
-	
-	private int[] readConfigFromFile(String filename) 
-			throws FileNotFoundException, ReadJobsFirstException, InvalidFileFormatException {
-		if(jobs == null || jobs.length == 0) {
-			throw new ReadJobsFirstException();
-		}
-		int[] res = new int[jobs.length];
-		int[] posToId = new int[jobs.length];
-		
-		BufferedReader reader = new BufferedReader(new FileReader(filename));
-		
-		try {
-			int lineNo = 0;
-			for(int i = 0; i < jobs.length; i++) {
-				String line;
-				do {
-					line = reader.readLine();
-					lineNo++;
-					if(line == null) {
-						throw new InvalidFileFormatException(res.length + 
-								" jobs are required but " + filename + " contains only " + i);
-					}
-					line = line.trim();
-				} while(line.isEmpty() || line.charAt(0) == '#');
-				
-				StringTokenizer tok = new StringTokenizer(line);
-				if(tok.countTokens() != 2) {
-					throw new InvalidFileFormatException("invalid file format in file " + 
-							filename + " in line " + lineNo + ": " + line);
-				}
-				
-				int id;
-				try {
-					id = Integer.parseInt(tok.nextToken());
-				} catch (NumberFormatException e) {
-					throw new InvalidFileFormatException("in file " + filename + " in line " + lineNo + ": " + e.getMessage());
-				}
-				if(id <= 0 || id > res.length) {
-					throw new InvalidFileFormatException("invalid job id in file " + 
-							filename + " in line " + lineNo + ": " + line);
-				}
-				if(res[id-1] != 0) {
-					throw new InvalidFileFormatException("job id " + id + " appears more than once in file " + filename);
-				}
-				
-				int pos;
-				try {
-					pos = Integer.parseInt(tok.nextToken());
-				} catch (NumberFormatException e) {
-					throw new InvalidFileFormatException("in file " + filename + " in line " + lineNo + ": " + e.getMessage());
-				}
-				if(pos <= 0 || pos > res.length) {
-					throw new InvalidFileFormatException("invalid job position in file " + 
-							filename + " in line " + lineNo + ": " + line);
-				}
-				if(posToId[pos-1] != 0) {
-					throw new InvalidFileFormatException("job position " + pos + " appears more than once in file " + filename);
-				}
-				
-				res[id-1] = pos;
-				posToId[pos-1] = id;
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if(reader != null) {
-				try {
-					reader.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		return res;
-	}
 	
 	public SynmvFrame() throws FileNotFoundException {
 		super();
@@ -276,26 +194,16 @@ public class SynmvFrame extends JFrame {
 		scroll.setViewportView(jobcontainer);
 		this.add(scroll);
 
-		
-		jobcontainer.add(loadFile);
-		loadFile.setLocation(200, 400);
-		loadFile.setSize(150, 40);
-		
-		jobcontainer.add(loadConfig);
-		loadConfig.setLocation(400, 400);
-		loadConfig.setSize(150, 40);
-		
-		jobcontainer.add(storeConfig);
-		storeConfig.setLocation(600, 400);
-		storeConfig.setSize(150,  40);
+		this.add(menubar, BorderLayout.NORTH);
+		menubar.add(fileMenu);
+		fileMenu.add(loadFile);
+		fileMenu.add(storeFile);
 		
 		jobcontainer.add(label);
 		label.setLocation(20, 400);
 		label.setSize(280, 15);
-
-		
+	
 		fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
-		configChooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
 
 		loadFile.addActionListener(new ActionListener() {
 			@Override
@@ -343,7 +251,7 @@ public class SynmvFrame extends JFrame {
 			}
 		});
 		
-		storeConfig.addActionListener(new ActionListener() {
+		storeFile.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -351,48 +259,9 @@ public class SynmvFrame extends JFrame {
 					JOptionPane.showMessageDialog(SynmvFrame.this, "there is nothing to store");
 					return;
 				}
-				int ret = configChooser.showSaveDialog(SynmvFrame.this);
+				int ret = fileChooser.showSaveDialog(SynmvFrame.this);
 				if(ret == JFileChooser.APPROVE_OPTION) {
-					storeConfigToFile(configChooser.getSelectedFile().getAbsolutePath());
-				}
-			}
-		});
-		
-		loadConfig.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				if(jobs == null || jobs.length == 0) {
-					JOptionPane.showMessageDialog(SynmvFrame.this, "read jobs file first");
-					return;
-				}
-				
-				int ret = configChooser.showOpenDialog(SynmvFrame.this);
-				if(ret == JFileChooser.APPROVE_OPTION) {
-					int[] config = null;
-					try {
-						config = readConfigFromFile(configChooser.getSelectedFile().getAbsolutePath());
-					} catch (FileNotFoundException e) {
-						JOptionPane.showMessageDialog(SynmvFrame.this, e.getMessage(), "file not found", JOptionPane.ERROR_MESSAGE);
-					} catch (ReadJobsFirstException e) {
-						JOptionPane.showMessageDialog(SynmvFrame.this, "read jobs file first");
-					} catch (InvalidFileFormatException e) {
-						JOptionPane.showMessageDialog(SynmvFrame.this, e.getMessage(), "invalide file format", JOptionPane.ERROR_MESSAGE);
-					}
-					
-					if(config != null) {
-						SynmvJob[] newJobs = new SynmvJob[jobs.length];
-						for(int i = 0; i < jobs.length; i++) {
-							jobs[i].unlink();
-							newJobs[config[jobs[i].getID()-1]-1] = jobs[i];
-						}
-						
-						jobs = newJobs;
-						for(int i = 1; i < jobs.length; i++) {
-							jobs[i].setPred(jobs[i-1]);
-							jobs[i-1].setNext(jobs[i]);
-						}
-					}
+					storeJobsToFile(fileChooser.getSelectedFile().getAbsolutePath());
 				}
 			}
 		});
