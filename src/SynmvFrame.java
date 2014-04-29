eb16f8bda98d12d2b0e609104c3a7de28d4c7e75
sebrockm <sebrockm@uos.de>
2014-04-29 16:11:47 +0200

    window does not automatically switch to default size any more

--- src/SynmvFrame.java
+++ src/SynmvFrame.java
@@ -325,7 +325,6 @@ public class SynmvFrame extends JFrame {
 									text += "    Lmax: " + lmax;
 								}
 								label.setText(text);
-								SynmvFrame.this.pack();
 							}
 						});
 					}
