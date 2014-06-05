b5e90b283f5752dacdacdfabd6890cb505d2386c
sebrockm <sebrockm@uos.de>
2014-06-05 22:07:57 +0200

    updated jar

--- src/SynmvFrame.java
+++ src/SynmvFrame.java
@@ -6,7 +6,6 @@ import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 import java.io.BufferedReader;
